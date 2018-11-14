9c011db78e9f135fb94263ddf561d31218a01c38
xnng <xnng77@gmail.com>
2018-11-14 11:54:34 +0800

    update

--- git-prompt.sh
+++ git-prompt.sh
@@ -11,10 +11,12 @@ then
 else
 	PS1='\[\033]0;Bash\007\]'      # 窗口标题
 	PS1="$PS1"'\n'                 # 换行
+	# PS1="$PS1"'\[\033[32m\]'     # 绿色
+	# PS1="$PS1"'\u '              # 用户名
+	# PS1="$PS1"'\[\033[0m\]'      # 灰色
+	# PS1="$PS1"'at '			   # at
 	PS1="$PS1"'\[\033[32m\]'       # 绿色
-	PS1="$PS1"'\u '            	   # 用户名
-	PS1="$PS1"'\[\033[0m\]'        # 灰色
-	PS1="$PS1"'at '				   # at
+	PS1="$PS1"'-- '
 	PS1="$PS1"'\[\033[35m\]'       # 粉红色
 	PS1="$PS1"'\t '				   # 时间
 	PS1="$PS1"'\[\033[33m\]'       # 黄色
@@ -29,7 +31,7 @@ else
 		then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+			PS1="$PS1"'\[\033[31m\]'  # change color to cyan
 			PS1="$PS1"'`__git_ps1`'   # bash function
 		fi
 	fi

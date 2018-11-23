feca522b7f96fb8dca5b994e0f4c940bd5afc28f
xnng <xnng77@gmail.com>
2018-11-23 16:00:52 +0800

    update

--- install.sh
+++ install.sh
@@ -31,5 +31,5 @@ bind -n WheelDownPane select-pane -t= \; send-keys -M" > ~/.tmux.conf \
 && explorer . \
 && cd ../ \
 && echo -e "please copy 'DejaVu Sans Mono for Powerline.tty' to 'C:/Windows/Fonts' by your hands \n
-I can't do this by terminal"
+I can't do this by terminal" \
 && echo "enjoy it !"
